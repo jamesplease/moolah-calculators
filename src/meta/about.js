70a7063a9d708e9a7239efbe6c244adadea583e3
James Smith <jamesplease2@gmail.com>
2020-07-23 00:19:41 -0700

    Add historical data source

--- src/meta/about.js
+++ src/meta/about.js
@@ -12,6 +12,16 @@ export default function About() {
         All of these calculators were created by me, James. It's nice to meet
         you!
       </p>
+      <h2 className="heading2">Historical Data Source</h2>
+      <p>
+        Some of the calculators on this website use historical data. All
+        historical data is from Nobel Prize-winning economist{" "}
+        <a href="https://en.wikipedia.org/wiki/Robert_J._Shiller">
+          Robert Shiller
+        </a>
+        . You can access this historical data on{" "}
+        <a href="http://www.econ.yale.edu/~shiller/data.htm">his website</a>.
+      </p>
     </div>
   );
 }
